1c9b3f9ee8032a24d3111d9114a04d0484361bfc
DevJola <paulakerejola27@gmail.com>
2022-08-08 19:57:30 +0100

    created dashboard

---
+++ fintech-payment-front-end-sqd11a-group-b/src/components/Navbar/index.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import {Nav, NavLink, StackLink, NameLink} from './NavbarElements'
+import { Stack, Badge, Avatar } from "@mui/material"
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import '../Navbar/navbar.css';
+
+
+const Navbar = () => {
+  return (
+    <>
+        <Nav>
+            <NavLink to ="/dashboard">
+                <p className='text'>Fintech.africa</p>
+            </NavLink>
+
+            <div className='notification-profile_container'>
+            <StackLink to ="/notifications"> 
+                <Stack>            
+                    <Badge badgeContent={1} color='error'>
+                        <NotificationsNoneIcon/>           
+                    </Badge>       
+                </Stack>
+            </StackLink> 
+
+            <NameLink to ="/profile">
+                <Avatar alt="Adetutu" src="" />
+                <h5 className ='name'>Adetutu</h5>
+            </NameLink>
+            </div>
+                       
+        </Nav>
+
+    </>
+  )
+}
+
+export default Navbar
