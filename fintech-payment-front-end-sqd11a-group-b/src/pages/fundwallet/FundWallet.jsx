9e51dc738bb9ae34d1c73ef871d4f1d12d716a95
obemeuche <obemeuchechi@gmail.com>
2022-08-30 23:38:48 +0100

    integrated with the backend

--- fintech-payment-front-end-sqd11a-group-b/src/pages/fundwallet/FundWallet.jsx
+++ fintech-payment-front-end-sqd11a-group-b/src/pages/fundwallet/FundWallet.jsx
@@ -1,10 +1,41 @@
-import React from "react";
-import {Link} from "react-router-dom";
+import React, { useRef } from "react";
+import { Link } from "react-router-dom";
 import Navbar from "../dashboard/Navbar/Navbar";
 import { BsArrowLeft} from 'react-icons/bs';
 import "./fundwallet.css";
+import axios from "axios";
+import swal from "sweetalert";
 
+// const token = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJvYmVtZXVjaGVjaGlAZ21haWwuY29tIiwiZXhwIjoxNjYxOTAyMjAzLCJpYXQiOjE2NjE4OTg2MDN9.C6GuB0qXsM11VCW9iE-UGQqu4Xsfgke3pZ2Cheo5awg"
 export default function FundWallet () {
+    const token = localStorage.getItem("token");
+
+    let submitRef = useRef(0);
+
+    const handleFunding = function (e) {
+        e.preventDefault();
+        const amount = submitRef.current.value;
+
+            (async ()=> {
+                const config = {
+                    headers: {
+                        Authorization: token,
+                        'Content-Type': 'application/json'
+                    }
+                }
+                try {
+                    const response = await axios.post("http://localhost:8085/api/v1/fundLocalWallet", {amount: amount}, config);
+                    await swal("Funding successful!", { appearance: "success"}).then(function() { window.location = "/dashboard"; });
+
+                } catch (error) {
+                    console.log(error);
+                    await swal("Invalid amount", {appearance: "error"});
+                }
+            })();
+
+    }
+
+
     return(
         <>
         <Navbar />
@@ -17,10 +48,8 @@ export default function FundWallet () {
                 </div>
                 <h4 className="fund--wallet-header">FUND WALLET</h4>
                 <div className="fund--wallet-form-body">
-                    <form className="fund--wallet-form">
-                        {/*<label htmlFor="" className="fund--wallet-form-header">Amount*/}
-                            <input type="text" className="fund--wallet-input" name="amount" placeholder="Amount"/>
-                        {/*</label>*/}
+                    <form className="fund--wallet-form" onSubmit={handleFunding}>
+                        <input type="text" className="fund--wallet-input" name="amount" placeholder="Amount" ref={submitRef}/>
                         <div className="fund--wallet-btn-container">
                             <button type="submit" className="fund--wallet-btn">Fund Wallet</button>
                         </div>
